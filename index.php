736b4398dbbcf57f662708f916ad20d7d3b821b4
Julius Robert Javier <juliusrobertjavier@gmail.com>
2022-06-10 19:44:23 +0800

    reworking on the output of arraysorting

--- index.php
+++ index.php
@@ -3,22 +3,8 @@
 
 	$arraysort = new ArraySort( array(1, 3, 5, 8, 7, 4, 2) );
 	echo "<br>";
-	print_r($arraysort);
-	echo "<br>"; 
-	print_r($arraysort->sort());
-	echo "<br>";
-
-	$arraysort = new ArraySort(array(1, 5, -4, 0, 7, 1, 2));
-	echo "<br>";
-	print_r($arraysort);
-	echo "<br>"; 
-	print_r($arraysort->sort());
-	echo "<br>";
-
-	$arraysort = new ArraySort(array());
-	echo "<br>";
-	print_r($arraysort);
-	echo "<br>"; 
-	print_r($arraysort->sort());
+	echo "Original Array: ";var_dump ($arraysort->array);
+	echo "<br> Sorted Array: "; 
+	var_dump($arraysort->sort());
 	echo "<br>";
 ?>
